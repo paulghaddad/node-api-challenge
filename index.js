8b54eeb0da8d13311954b8fc3943ee2feb99ce28
Paul Haddad <paulh16@gmail.com>
2020-07-27 13:57:27 -0400

    Finish Projects CRUD actions

--- index.js
+++ index.js
@@ -19,28 +19,75 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
 
 app.get("/projects", (req, res) => {
   projects
-    .get()
-    .then((projects) => {
-      return res.json(projects);
-    })
-    .catch((error) => {
-      console.log("error");
-    });
-});
+  .get()
+  .then((projects) => {
+    return res.json(projects)
+  })
+  .catch((error) => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
 
-app.route("/project/:project_id").get((req, res) => {
+app.route("/projects/:project_id").get((req, res) => {
   projects
-    .get(req.params.project_id)
-    .then((project) => {
-      if (!project) return res.status(404).json({ error: "No project found" });
+  .get(req.params.project_id)
+  .then((project) => {
+    if (!project) return res.status(404).json({ error: "No project found" })
 
-      return res.json(project);
-    })
-    .catch((error) => console.log("error"));
-});
+    return res.json(project)
+  })
+  .catch(error => {
+        return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.route('/projects').post((req, res) => {
+  const {name, description, completed} = req.body
+
+  projects
+  .insert({name, description, completed})
+  .then(project => {
+    return res.status(201).json(project)
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.route('/projects').put((req, res) => {
+  const {id, ...changes} = req.body
+
+  projects
+  .update(id, changes)
+  .then(project => {
+    if (!project)
+      return res.status(404).json({error: 'A project with that id was not found.'})
+
+    return res.json(project)
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.route('/projects/:project_id').delete((req, res) => {
+  const { project_id } = req.params
+
+  projects
+  .remove(project_id)
+  .then(count => {
+    if (!count)
+        return res.status(404).json({error: 'A project with that id was not found.'})
+
+    res.status(204).json()
+  })
+    .catch(error => res.status(500).json({error: 'There was an error processing your request'}))
+})
