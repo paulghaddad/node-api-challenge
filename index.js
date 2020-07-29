44013d07084026b2af32a301042d756ee2d2be51
Paul Haddad <paulh16@gmail.com>
2020-07-29 09:58:51 -0400

    Refactor project CRUD endpoints

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
-
-Sing along:
-
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
-
-Go code!
-*/
-
 const projects = require("./data/helpers/projectModel.js");
 
 const express = require("express");
@@ -36,9 +21,11 @@ app.get("/projects", (req, res) => {
   })
 })
 
-app.route("/projects/:project_id").get((req, res) => {
+app.get("/projects/:project_id", (req, res) => {
+  const id = req.params.project_id
+
   projects
-  .get(req.params.project_id)
+  .get(id)
   .then((project) => {
     if (!project) return res.status(404).json({ error: "No project found" })
 
@@ -49,7 +36,7 @@ app.route("/projects/:project_id").get((req, res) => {
   })
 })
 
-app.route('/projects').post((req, res) => {
+app.post('/projects', (req, res) => {
   const {name, description, completed} = req.body
 
   projects
@@ -62,8 +49,9 @@ app.route('/projects').post((req, res) => {
   })
 })
 
-app.route('/projects').put((req, res) => {
-  const {id, ...changes} = req.body
+app.put('/projects/:project_id', (req, res) => {
+  const id = req.params.project_id
+  const changes = req.body
 
   projects
   .update(id, changes)
@@ -74,20 +62,24 @@ app.route('/projects').put((req, res) => {
     return res.json(project)
   })
   .catch(error => {
+    console.log(error)
     return res.status(500).json({error: 'There was an error processing your request'})
   })
 })
 
-app.route('/projects/:project_id').delete((req, res) => {
-  const { project_id } = req.params
+app.delete('/projects/:project_id', (req, res) => {
+  const id = req.params.project_id
 
   projects
-  .remove(project_id)
+  .remove(id)
   .then(count => {
     if (!count)
         return res.status(404).json({error: 'A project with that id was not found.'})
 
     res.status(204).json()
   })
-    .catch(error => res.status(500).json({error: 'There was an error processing your request'}))
+  .catch(error => {
+console.log(error)
+    res.status(500).json({error: 'There was an error processing your request'})
+  })
 })
