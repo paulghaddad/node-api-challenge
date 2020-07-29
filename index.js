d5ad3ca5366ee381a029ea8415b1b39da4723553
Paul Haddad <paulh16@gmail.com>
2020-07-29 15:36:30 -0400

    Finish out actions endpoints

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const projects = require("./data/helpers/projectModel.js");
+const actions = require("./data/helpers/actionModel.js");
 
 const express = require("express");
 const app = express();
@@ -76,10 +77,125 @@ app.delete('/projects/:project_id', (req, res) => {
     if (!count)
         return res.status(404).json({error: 'A project with that id was not found.'})
 
-    res.status(204).json()
+    return res.status(204).json()
   })
   .catch(error => {
 console.log(error)
     res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.get('/projects/:project_id/actions', (req, res) => {
+  const project_id = req.params.project_id
+
+  // confirm project exists
+  projects
+  .get(project_id)
+  .then(project => {
+    if (!project)
+          return res.status(404).json({error: 'A project with that id was not found.'})
+
+    projects
+    .getProjectActions(project_id)
+    .then(actions => {
+      return res.json({project_id, actions})
+    })
+    .catch(error => {
+        return res.status(500).json({error: 'There was an error processing your request'})
+    })
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+
+})
+
+app.get('/actions', (req, res) => {
+  actions.get()
+  .then(actions => {
+    return res.json(actions)
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.get('/actions/:action_id', (req, res) => {
+  const action_id = req.params.action_id
+
+  actions.get(action_id)
+  .then(action => {
+    if (!action)
+        return res.status(404).json({error: 'A action with that id was not found.'})
+
+    return res.json(action)
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.post('/actions', (req, res) => {
+  const action_data = req.body
+
+  projects
+  .get(action_data.project_id)
+  .then(project => {
+    // Ensure the project exists
+    if (!project)
+        return res.status(404).json({error: 'A project with that id was not found.'})
+
+    // create action
+    actions
+    .insert(action_data)
+    .then(action => {
+      return res.json(action)
+    })
+    .catch(error => {
+      return res.status(500).json({error: 'There was an error processing your request'})
+    })
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.put('/actions/:action_id', (req, res) => {
+  const action_id = req.params.action_id
+  const {description, notes, completed} = req.body
+
+  actions
+  .get(action_id)
+  .then(action => {
+    actions
+    .update(action_id, {description, notes, completed})
+    .then(action => {
+      if (!action)
+        return res.status(404).json({error: 'An action with that id was not found.'})
+
+      return res.json(action)
+    })
+    .catch(error => {
+      return res.status(500).json({error: 'There was an error processing your request'})
+    })
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
+  })
+})
+
+app.delete('/actions/:action_id', (req, res) => {
+  const action_id = req.params.action_id
+
+  actions
+  .remove(action_id)
+  .then(count => {
+    if (!count)
+        return res.status(404).json({error: 'An action with that id was not found.'})
+
+    return res.status(204).json()
+  })
+  .catch(error => {
+    return res.status(500).json({error: 'There was an error processing your request'})
   })
 })
